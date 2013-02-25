2c84ae97c8c34b0236bef1890cf802d33a71427d
kecinzer <kecinzer@gmail.com>
2013-02-25 21:28:19 +0100

    Included needed library into Android.mk
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13 android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13 android-support-v4 xposed
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
@@ -11,6 +11,10 @@ LOCAL_PACKAGE_NAME := XposedAppSettings
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := xposed:lib/XposedBridgeApi-2.1.jar
+
+include $(BUILD_MULTI_PREBUILT)
 
 # Use the folloing include to make our test apk.
 include $(call all-makefiles-under,$(LOCAL_PATH))
